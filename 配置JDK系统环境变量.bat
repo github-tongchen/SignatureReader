33a1306ae5e1fa7955195df0c8c240d1be58f3c9
Tong Chen <839706407@qq.com>
2022-02-18 16:16:32 +0800

    Update 配置JDK系统环境变量.bat
    修复javac无效问题

--- 配置JDK系统环境变量.bat
+++ 配置JDK系统环境变量.bat
@@ -1,8 +1,8 @@
 @echo off
-echo 按回车键开始配置JAVA环境变量
+echo 鎸夊洖杞﹂敭寮�濮嬮厤缃甁AVA鐜鍙橀噺
 pause
 wmic ENVIRONMENT create name="JAVA_HOME",username="<system>",VariableValue="C:\Program Files\Java\jdk1.8.0_201\"
-wmic ENVIRONMENT create name="CLASSPATH",username="<system>",VariableValue=".;%%JAVA_HOME%%\lib;%%JAVA_HOME%%\lib\tools.jar" 
+wmic ENVIRONMENT create name="CLASSPATH",username="<system>",VariableValue=".;%%JAVA_HOME%%\bin;%%JAVA_HOME%%\lib\tools.jar" 
 wmic ENVIRONMENT where "name='PATH' and username='<system>'" set VariableValue="%path%;%%JAVA_HOME%%\bin;%%JAVA_HOME%%\jre\bin;" 
-echo 环境变量配置完成
+echo 鐜鍙橀噺閰嶇疆瀹屾垚
 pause
